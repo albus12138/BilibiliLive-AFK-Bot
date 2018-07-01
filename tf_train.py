5f3c8f5948dc6defa31b73f87f7d14156cb95eae
Raphael <albus.zly@gmail.com>
2018-07-01 09:08:47 +0800

    Finish TensorFlow CNN Train

--- tf_train.py
+++ tf_train.py
@@ -1,6 +1,8 @@
 # coding:utf-8
 import os
 import base64
+import string
+import random
 import shutil
 import requests
 import numpy as np
@@ -12,57 +14,73 @@ from utils import Bilibili
 HEIGHT = 40
 WIDTH = 120
 CAPTCHA_LENGTH = 4
+FILENAME_DICT = string.ascii_letters+string.digits
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
-def get_train_data(num, access_token):
-    url = 'https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic?access_token=' + access_token
-    # 二进制方式打开图文件
+def __gen_random_name():
+    return "tmp/" + "".join(random.sample(FILENAME_DICT, 8)) + ".bmp"
+
+
+def get_bilibili_captcha(num):
+    global bilibili
     ans = 0
-    cookies = {
-        "LIVE_BUVID": "AUTO7415301873495121",
-        "finger": "49387dad",
-        "buvid3": "CADB6203-7531-474F-BF03-A6A9E7E13D4965571infoc",
-        "sid": "jxlhdren",
-        "fts": "1530187377",
-        "DedeUserID": "346167248",
-        "DedeUserID__ckMd5": "bd5ef2b809d85a33",
-        "SESSDATA": "4208db4f%2C1532781480%2C8a1969c1",
-        "bili_jct": "fe4c568af7eaddb752ba4bb34e4265bc",
-        "_dfcaptcha": "61531516def9bcddd5fb0fee61445f25"
-    }
-    bilibili = Bilibili(cookies)
-    files = []
     while ans < num:
-        flag = True
         status = bilibili._get_captcha()
-        if not status:
-            continue
-        bilibili._ocr()
-        rfile = open(r'.tmp.bmp', 'rb')
-        # 参数image：图像base64编码
-        img = base64.b64encode(rfile.read())
-        rfile.close()
-        params = {"image": img}
-        req = requests.post(url, data=params, headers={'Content-Type': 'application/x-www-form-urlencoded'})
-        content = req.json()["words_result"]
-        if len(content) == 0:
-            continue
-        content = content[0]["words"]
-        if content:
-            if len(content) != 4:
+        if status:
+            bilibili._ocr()
+            shutil.move(".tmp.bmp", __gen_random_name())
+            ans += 1
+
+
+def baidu_ocr(num):
+    global access_token
+    url = 'https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic?access_token=' + access_token
+    if os.path.exists("list.txt"):
+        with open("list.txt", "r") as rfile:
+            line = rfile.readlines()
+            ans = int(line[-1].split(" ")[0]) if len(line) != 0 else 0
+    else:
+        os.mknod("list.txt")
+        ans = 0
+    while ans < num:
+        print("Processing {}/{}".format(ans, num))
+        files = os.listdir("tmp")
+        if len(files) == 0:
+            get_bilibili_captcha(100)
+            files = os.listdir("tmp")
+        for file in files:
+            flag = True
+            filename = os.path.join("tmp", file)
+            with open(filename, "rb") as rfile:
+                img = base64.b64encode(rfile.read())
+            res = requests.post(url, data={"image": img}, headers={'Content-Type': 'application/x-www-form-urlencoded'})
+            content = res.json()["words_result"]
+            if len(content) == 0:
                 continue
-            for i in range(0, 4):
-                if i == 2 and content[i] not in "+-":
-                    flag = False
-                if i != 2 and content[i] not in "1234567890":
-                    flag = False
-            if flag:
-                print("{}/{} | {}".format(ans, num, content))
-                shutil.copy(".tmp.bmp", "train_data/{}.bmp".format(content))
-                files.append("{}.bmp".format(content))
-                ans += 1
-    return files
+            content = content[0]["words"]
+            if content:
+                if len(content) != 4:
+                    continue
+                for i in range(0, 4):
+                    if i == 2 and content[i] not in "+-":
+                        flag = False
+                    if i != 2 and content[i] not in "1234567890":
+                        flag = False
+                if flag:
+                    shutil.copy(filename, "train_data/{}.bmp".format(ans))
+                    with open("list.txt", "a") as wfile:
+                        wfile.write("{} {}\n".format(ans, content))
+                    ans += 1
+        for file in files:
+            os.remove(os.path.join("tmp", file))
+
+
+def get_train_data(num):
+    rfile = open("list.txt", "r")
+    lines = rfile.readlines()
+    rfile.close()
+    return random.sample(lines, num)
 
 
 def text2vector(text):
@@ -100,11 +118,11 @@ def get_train_batch(size=64):
     global files
     batch_x = np.zeros([size, HEIGHT*WIDTH])
     batch_y = np.zeros([size, 48])
-    files = get_train_data(size, access_token)
+    files = get_train_data(size)
     for i in range(0, size):
         file = files.pop()
-        text = file.split(".")[0]
-        image = io.imread(os.path.join("train_data", file), as_grey=True)
+        text = file.split(" ")[1]
+        image = io.imread(os.path.join("train_data", "{}.bmp".format(file.split(" ")[0])), as_grey=True)
         batch_x[i, :] = image.flatten()
         batch_y[i, :] = text2vector(text)
     return batch_x, batch_y
@@ -112,12 +130,6 @@ def get_train_batch(size=64):
 
 def crack_captcha_cnn(w_alpha=0.01, b_alpha=0.1):
     x = tf.reshape(X, shape=[-1, HEIGHT, WIDTH, 1])
-
-    # w_c1_alpha = np.sqrt(2.0/(IMAGE_HEIGHT*IMAGE_WIDTH)) #
-    # w_c2_alpha = np.sqrt(2.0/(3*3*32))
-    # w_c3_alpha = np.sqrt(2.0/(3*3*64))
-    # w_d1_alpha = np.sqrt(2.0/(8*32*64))
-    # out_alpha = np.sqrt(2.0/1024)
 
     # 3 conv layer
     w_c1 = tf.Variable(w_alpha * tf.random_normal([3, 3, 1, 32]))
@@ -154,11 +166,7 @@ def crack_captcha_cnn(w_alpha=0.01, b_alpha=0.1):
 
 def train_crack_captcha_cnn():
     output = crack_captcha_cnn()
-    # loss
-    # loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(output, Y))
     loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=output, labels=Y))
-    # 最后一层用来分类的softmax和sigmoid有什么不同？
-    # optimizer 为了加快训练 learning_rate应该开始大，然后慢慢衰
     optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
 
     predict = tf.reshape(output, [-1, CAPTCHA_LENGTH, 12])
@@ -169,38 +177,77 @@ def train_crack_captcha_cnn():
 
     saver = tf.train.Saver()
     with tf.Session() as sess:
-        if os.path.exists("my_model/bilibili_captcha.model"):
-            saver.restore(sess, "my_model/bilibili_captcha.model")
+        step = 0
+        if os.path.exists("my_model/checkpoint"):
+            checkpoint = open("my_model/checkpoint", "r")
+            record = checkpoint.readline()
+            saver.restore(sess, "my_model/{}".format(record.split("\"")[1]))
+            step = int(record.split("\"")[1].split("-")[-1])
+            checkpoint.close()
+            print("Successful load checkpoint {}".format(step))
         else:
             sess.run(tf.global_variables_initializer())
 
-        step = 0
         while True:
             batch_x, batch_y = get_train_batch(64)
             _, loss_ = sess.run([optimizer, loss], feed_dict={X: batch_x, Y: batch_y, keep_prob: 0.75})
             print(step, loss_)
 
-            # 每100 step计算一次准确率
-            if step % 100 == 0 and step != 0:
+            if step % 50 == 0:
                 batch_x_test, batch_y_test = get_train_batch(100)
                 acc = sess.run(accuracy, feed_dict={X: batch_x_test, Y: batch_y_test, keep_prob: 1.})
                 print(step, acc)
-                # 如果准确率大于50%,保存模型,完成训练
-                if acc > 0.5:
+                if acc > 0.8:
                     break
             saver.save(sess, "my_model/bilibili_captcha.model", global_step=step)
             step += 1
 
 
+def test_cnn_accuracy():
+    output = crack_captcha_cnn()
+
+    predict = tf.reshape(output, [-1, CAPTCHA_LENGTH, 12])
+    max_idx_p = tf.argmax(predict, 2)
+    max_idx_l = tf.argmax(tf.reshape(Y, [-1, CAPTCHA_LENGTH, 12]), 2)
+    correct_pred = tf.equal(max_idx_p, max_idx_l)
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        if os.path.exists("my_model/checkpoint"):
+            checkpoint = open("my_model/checkpoint", "r")
+            record = checkpoint.readline()
+            saver.restore(sess, "my_model/{}".format(record.split("\"")[1]))
+            step = int(record.split("\"")[1].split("-")[-1])
+            checkpoint.close()
+            print("Successful load checkpoint {}".format(step))
+        else:
+            print("Checkpoint not found.")
+            return 0
+
+        batch_x_test, batch_y_test = get_train_batch(100)
+        acc = sess.run(accuracy, feed_dict={X: batch_x_test, Y: batch_y_test, keep_prob: 1.})
+        print(step, acc)
+
+
 if __name__ == "__main__":
-    ak = "noCVRz1mELyT1aCDcIOGHBI5"
-    sk = "6SDEYTNZaMMOG0tlEk93muSqmWfA0z1Y"
-    host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={}&client_secret={}'.format(ak, sk)
-    req = requests.post(host, headers={'Content-Type': 'application/json; charset=UTF-8'})
-    access_token = req.json()["access_token"]
-
-    X = tf.placeholder(tf.float32, [None, HEIGHT * WIDTH])
-    Y = tf.placeholder(tf.float32, [None, 48])
-    keep_prob = tf.placeholder(tf.float32)
-
-    train_crack_captcha_cnn()
+    mode = 2
+    if mode == 0:
+        cookies = {}  # bilibili cookies, key: value
+        bilibili = Bilibili(cookies)
+        ak = ""  # baidu ai application key
+        sk = ""  # baidu ai application secret key
+        host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={}&client_secret={}'.format(ak, sk)
+        req = requests.post(host, headers={'Content-Type': 'application/json; charset=UTF-8'})
+        access_token = req.json()["access_token"]
+        baidu_ocr(101)
+    elif mode == 1:
+        X = tf.placeholder(tf.float32, [None, HEIGHT * WIDTH])
+        Y = tf.placeholder(tf.float32, [None, 48])
+        keep_prob = tf.placeholder(tf.float32)
+        train_crack_captcha_cnn()
+    elif mode == 2:
+        X = tf.placeholder(tf.float32, [None, HEIGHT * WIDTH])
+        Y = tf.placeholder(tf.float32, [None, 48])
+        keep_prob = tf.placeholder(tf.float32)
+        test_cnn_accuracy()
